bdc8c71da6c31d15cf0ecd8461a753515b817ac3
William Peregoy <williamperegoy@gmail.com>
2017-10-02 12:19:16 -0700

    Initial Commit

---
+++ Take 10 Photos & Store Them/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Take 10 Photos & Store Them
+//
+//  Created by William Peregoy on 10/2/17.
+//  Copyright © 2017 William Peregoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
